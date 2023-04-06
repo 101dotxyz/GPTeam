aca1923958d948824d251c3ce20347703bda2569
Joshua Snyder <joshua@timenavi.com>
2023-04-06 16:36:45 +0100

    WIP extending ConversationalChatAgent

--- 101_ai/agents/task_definition.py
+++ 101_ai/agents/task_definition.py
@@ -2,10 +2,10 @@
 from __future__ import annotations
 
 import json
-from typing import Any, List, Optional, Sequence, Tuple
+from typing import Any, List, Optional, Sequence, Tuple, Union
 
 from langchain import SerpAPIWrapper
-from langchain.agents import AgentExecutor, Tool
+from langchain.agents import AgentExecutor, ConversationalChatAgent, Tool
 from langchain.agents.agent import Agent
 from langchain.agents.conversational_chat.prompt import (
     FORMAT_INSTRUCTIONS, PREFIX, SUFFIX, TEMPLATE_TOOL_RESPONSE)
@@ -18,8 +18,9 @@ from langchain.prompts.chat import (ChatPromptTemplate,
                                     HumanMessagePromptTemplate,
                                     MessagesPlaceholder,
                                     SystemMessagePromptTemplate)
-from langchain.schema import (AgentAction, AIMessage, BaseLanguageModel,
-                              BaseMessage, BaseOutputParser, HumanMessage)
+from langchain.schema import (AgentAction, AgentFinish, AIMessage,
+                              BaseLanguageModel, BaseMessage, BaseOutputParser,
+                              HumanMessage)
 from langchain.tools.base import BaseTool
 
 from ..tools.user_input import UserInputTool
@@ -47,122 +48,42 @@ class AgentOutputParser(BaseOutputParser):
         return {"action": response["action"], "action_input": response["action_input"]}
 
 
-class TaskDefinitionAgent(Agent):
-    """An agent designed to hold a conversation in addition to using tools."""
+class TaskDefinitionAgent(ConversationalChatAgent):
+    def plan(
+        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
+    ) -> Union[AgentAction, AgentFinish]:
+        """Given input, decided what to do.
 
-    output_parser: BaseOutputParser
+        Args:
+            intermediate_steps: Steps the LLM has taken to date,
+                along with observations
+            **kwargs: User inputs.
 
-    @property
-    def _agent_type(self) -> str:
-        raise NotImplementedError
+        Returns:
+            Action specifying what tool to use.
+        """
 
-    @property
-    def observation_prefix(self) -> str:
-        """Prefix to append the observation with."""
-        return "Observation: "
 
-    @property
-    def llm_prefix(self) -> str:
-        """Prefix to append the llm call with."""
-        return "Thought:"
-    
-    def _get_next_action(self, full_inputs: dict[str, str]) -> AgentAction:
-        full_output = self.llm_chain.predict(**full_inputs)
-        parsed_output = self._extract_tool_and_input(full_output)
-        while parsed_output is None:
-            full_output = self._fix_text(full_output)
-            full_inputs["agent_scratchpad"] += full_output
-            output = self.llm_chain.predict(**full_inputs)
-            full_output += output
-            parsed_output = self._extract_tool_and_input(full_output)
-        return AgentAction(
-            tool=parsed_output[0], tool_input=parsed_output[1], log=full_output
-        )
+        suggested_action = super().plan(intermediate_steps, **kwargs)
+        
+        if isinstance(suggested_action, AgentFinish):
+            print("Agent is suggesting we finish the conversation. Initiating reflection instead.")
+        return suggested_action
 
-    @classmethod
-    def create_prompt(
-        cls,
-        tools: Sequence[BaseTool],
-        system_message: str = PREFIX,
-        human_message: str = SUFFIX,
-        input_variables: Optional[List[str]] = None,
-        output_parser: Optional[BaseOutputParser] = None,
-    ) -> BasePromptTemplate:
-        tool_strings = "\n".join(
-            [f"> {tool.name}: {tool.description}" for tool in tools]
-        )
-        tool_names = ", ".join([tool.name for tool in tools])
-        _output_parser = output_parser or AgentOutputParser()
-        format_instructions = human_message.format(
-            format_instructions=_output_parser.get_format_instructions()
-        )
-        final_prompt = format_instructions.format(
-            tool_names=tool_names, tools=tool_strings
-        )
-        if input_variables is None:
-            input_variables = ["input", "chat_history", "agent_scratchpad"]
-        messages = [
-            SystemMessagePromptTemplate.from_template(system_message),
-            MessagesPlaceholder(variable_name="chat_history"),
-            HumanMessagePromptTemplate.from_template(final_prompt),
-            MessagesPlaceholder(variable_name="agent_scratchpad"),
-        ]
-        return ChatPromptTemplate(input_variables=input_variables, messages=messages)
+    async def aplan(
+        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
+    ) -> Union[AgentAction, AgentFinish]:
+        """Given input, decided what to do.
 
-    def _extract_tool_and_input(self, llm_output: str) -> Optional[Tuple[str, str]]:
-        try:
-            response = self.output_parser.parse(llm_output)
-            return response["action"], response["action_input"]
-        except Exception:
-            raise ValueError(f"Could not parse LLM output: {llm_output}")
+        Args:
+            intermediate_steps: Steps the LLM has taken to date,
+                along with observations
+            **kwargs: User inputs.
 
-    def _construct_scratchpad(
-        self, intermediate_steps: List[Tuple[AgentAction, str]]
-    ) -> List[BaseMessage]:
-        """Construct the scratchpad that lets the agent continue its thought process."""
-        thoughts: List[BaseMessage] = []
-        for action, observation in intermediate_steps:
-            thoughts.append(AIMessage(content=action.log))
-            human_message = HumanMessage(
-                content=TEMPLATE_TOOL_RESPONSE.format(observation=observation)
-            )
-            thoughts.append(human_message)
-        return thoughts
-
-    @classmethod
-    def from_llm_and_tools(
-        cls,
-        llm: BaseLanguageModel,
-        tools: Sequence[BaseTool],
-        callback_manager: Optional[BaseCallbackManager] = None,
-        system_message: str = PREFIX,
-        human_message: str = SUFFIX,
-        input_variables: Optional[List[str]] = None,
-        output_parser: Optional[BaseOutputParser] = None,
-        **kwargs: Any,
-    ) -> Agent:
-        """Construct an agent from an LLM and tools."""
-        cls._validate_tools(tools)
-        _output_parser = output_parser or AgentOutputParser()
-        prompt = cls.create_prompt(
-            tools,
-            system_message=system_message,
-            human_message=human_message,
-            input_variables=input_variables,
-            output_parser=_output_parser,
-        )
-        llm_chain = LLMChain(
-            llm=llm,
-            prompt=prompt,
-            callback_manager=callback_manager,
-        )
-        tool_names = [tool.name for tool in tools]
-        return cls(
-            llm_chain=llm_chain,
-            allowed_tools=tool_names,
-            output_parser=_output_parser,
-            **kwargs,
-        )
+        Returns:
+            Action specifying what tool to use.
+        """
+        return AgentAction(tool="Search", tool_input="foo", log="")
 
 
 def get_task_definition_agent():

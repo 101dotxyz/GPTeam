2889dbc004dc245afa99f7a85215a19b5ba9daa3
Joshua Snyder <joshua@timenavi.com>
2023-04-06 17:16:40 +0100

    setup llm function tool

--- 101_ai/utils/chat.py
+++ 101_ai/utils/chat.py
@@ -7,7 +7,7 @@ from langchain.schema import BaseMessage
 from .models import ChatModel
 
 
-async def get_chat_completion(messages: list[BaseMessage], model: ChatModel):
+def get_chat_completion(messages: list[BaseMessage], model: ChatModel) -> str:
    chat = ChatOpenAI(model=model.value)
    response = chat(messages)
 

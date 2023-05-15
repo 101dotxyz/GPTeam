3ccf96def42d2be90e9dc3ca32b59377ae46bc54
Joshua Snyder <joshua@timenavi.com>
2023-05-15 16:59:33 +0100

    move quart to web folder

---
+++ src/web/__init__.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from quart import Quart, Response, render_template, send_file
+
+load_dotenv()
+
+app = Quart(__name__)
+
+
+def run() -> None:
+    app.run()
+
+
+@app.route("/")
+async def view_logs():
+    return await render_template("logs.html")
+
+
+# @app.route("/logs")
+# async def display_logs():
+#     print("event_stream requested")
+
+#     async def event_stream():
+#         with open("src/logs/agent.txt", "r") as log_file:
+#             while True:
+#                 line = log_file.readline()
+#                 if line:
+#                     yield f"data: {line}\n\n"
+
+#     return Response(event_stream(), mimetype="text/event-stream")

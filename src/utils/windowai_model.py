29fa1420590f16a9726d9686ee684aa93db3efa6
Joshua Snyder <joshua@timenavi.com>
2023-06-08 19:26:13 +0100

    remove console logs, add types

--- src/utils/windowai_model.py
+++ src/utils/windowai_model.py
@@ -1,6 +1,5 @@
 import langchain
 from langchain.chat_models.base import BaseChatModel, SimpleChatModel
-from langchain.schema import BaseMessage
 from langchain.schema import (
     AIMessage,
     BaseMessage,
@@ -8,18 +7,33 @@ from langchain.schema import (
     ChatResult,
     HumanMessage,
     SystemMessage,
-    LLMResult,
-    PromptValue,
 )
-from typing import Any, Dict, List, Mapping, Optional, Sequence
+from typing import Any, Dict, List, Mapping, Optional, Sequence, TypedDict
 import websocket
 import uuid
 import json
 
+
+class MessageDict(TypedDict):
+    role: str
+    content: str
+
+
+class RequestDict(TypedDict):
+    messages: List[MessageDict]
+    temperature: float
+    request_id: str
+
+
+class ResponseDict(TypedDict):
+    content: str
+    request_id: str
+
+
 class ChatWindowAI(BaseChatModel):
     model_name: str = "window"
     """Model name to use."""
-    temperature: float = 0.7
+    temperature: float = 0
     """What sampling temperature to use."""
     streaming: bool = False
     """Whether to stream the results."""
@@ -30,7 +44,7 @@ class ChatWindowAI(BaseChatModel):
     def _llm_type(self) -> str:
         """Return type of chat model."""
         return "window-chat"
-    
+
     def _generate(
         self, messages: List[BaseMessage], stop: Optional[List[str]] = None
     ) -> ChatResult:
@@ -39,7 +53,7 @@ class ChatWindowAI(BaseChatModel):
         generation = ChatGeneration(message=message)
         result = ChatResult(generations=[generation])
         return result
-    
+
     async def _agenerate(
         self, messages: List[BaseMessage], stop: Optional[List[str]] = None
     ) -> ChatResult:
@@ -49,14 +63,14 @@ class ChatWindowAI(BaseChatModel):
         self, messages: List[BaseMessage], stop: Optional[List[str]] = None
     ) -> str:
         request_id = str(uuid.uuid4())
-        request = {
+        request: RequestDict = {
             "messages": [],
             "temperature": self.temperature,
             "request_id": request_id,
         }
 
         for message in messages:
-            role = "user" # default role is user
+            role = "user"  # default role is user
             if isinstance(message, HumanMessage):
                 role = "user"
             elif isinstance(message, AIMessage):
@@ -64,10 +78,12 @@ class ChatWindowAI(BaseChatModel):
             elif isinstance(message, SystemMessage):
                 role = "system"
 
-            request["messages"].append({
-                "role": role,
-                "content": message.content,
-            })
+            request["messages"].append(
+                {
+                    "role": role,
+                    "content": message.content,
+                }
+            )
 
         ws = websocket.WebSocket()
         ws.connect("ws://127.0.0.1:5000/windowmodel")
@@ -75,13 +91,15 @@ class ChatWindowAI(BaseChatModel):
         message = ws.recv()
         ws.close()
 
-        message = json.loads(message)
+        response: ResponseDict = json.loads(message)
 
-        response_content = message["content"]
-        response_request_id = message["request_id"]
+        response_content = response["content"]
+        response_request_id = response["request_id"]
 
         # sanity check that response corresponds to request
         if request_id != response_request_id:
-            raise ValueError(f"Invalid request ID: {response_request_id}, expected: {request_id}")
+            raise ValueError(
+                f"Invalid request ID: {response_request_id}, expected: {request_id}"
+            )
 
         return response_content

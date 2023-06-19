e0f63d3132e921ebedcc254e0d6aff28b290b626
ryderwishart <ryderwishart@gmail.com>
2023-06-19 11:16:27 -0700

    Set ACTIVE_PORT once an open port has been found
    Subsequent calls to `get_open_port()` will use the open port that was initially found on app startup

--- src/utils/general.py
+++ src/utils/general.py
@@ -23,11 +23,21 @@ def seed_uuid(seed: str) -> str:
 
 def is_port_in_use(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex(("localhost", port)) == 0
+        return s.connect_ex(("localhost", port)) == 0  # TODO: use base URL from env
 
 
 def get_open_port():
-    port = os.getenv("PORT", "5000")
+    # check ACTIVE_PORT first
+    port = os.getenv("ACTIVE_PORT", None)
+    if port is not None:
+        print(f"Active port found: {port}")
+        return int(port)
+
+    port = int(os.getenv("PORT", "5023"))
     while is_port_in_use(port):
+        print(f"Port {port} is in use, trying next port...")
         port += 1
+
+    # set ACTIVE_PORT to the port we found
+    os.environ["ACTIVE_PORT"] = str(port)
     return port
